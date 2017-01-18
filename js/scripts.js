3ac680fb623544f731bc85923fb40c72c4a1a7ce
Charles Peden and Calvin Cushman <calvin@>
2017-01-18 11:55:57 -0800

    write out back end logic for js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,44 @@
 var testVowels = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U"]
 
-var vowelCounter = function(userInput) {
-  for(var index 0; index <= testVowels.length; index += 1) {
-    console.log("testVowels at " + testVowels[index]);
+// var filterFunction = function() {
+//   return value === testVowels[index];
+// }
 
-  }
-}
-
-var userInput = function(inputString) {
+var stringSplitter = function(inputString) {
   return inputString.split("");
 }
 
+
+// var isEqualto = function(currentVowel){
+  // return value === currentVowel;
+// }
+
+var vowelCounter = function(splitStrings) {
+ var currentLetters = splitStrings.map(function(splitString){
+   console.log("splitString is " + splitString);
+    for(var index = 0; index < testVowels.length; index += 1) {
+      console.log("testVowels at " + testVowels[index]);
+      var currentVowel = testVowels[index];
+      if (splitString === currentVowel )
+      return splitString;
+    };
+  });
+  currentLetters = currentLetters.filter(function(currentLetter){
+    return currentLetter;
+  });
+    console.log("currentLetters is " + currentLetters);
+  return currentLetters.length;
+};
 
 
 $(document).ready(function() {
   $("form#vowelFinder").submit(function(event) {
     event.preventDefault();
     var inputString = ($("input#inputString").val());
-    var result = userInput(inputString);
-    console.log(inputString.length);
-    $("#result").text(result);
+    var splitStrings = stringSplitter(inputString);
+    console.log("splitStrings set to " + splitStrings);
+    var vowelCount = vowelCounter(splitStrings);
+    console.log("vowelCount set to " + vowelCount)
+    $("#result").text(vowelCount);
   });
 });
